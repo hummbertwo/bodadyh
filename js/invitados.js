09be87dbc85054b9d6213132bdfa09ae77664a0d
hummbertwo <hummber.km@gmail.com>
2025-07-19 20:41:14 -0600

    hummber
    lista de invitados modificada

--- js/invitados.js
+++ js/invitados.js
@@ -1,9 +1,9 @@
 const codigos = {
-  "ELITE": ["Juan Pérez", "María López"],
-  "DELUXE": ["Carlos Ruiz", "Ana Martínez", "Luis Gómez"],
-  "GOLD": ["Lucía Torres"],
-  "PLATINUM": ["Marco Díaz", "Carmen Silva"],
-  "DIAMOND": ["Pedro Ramírez"],
-  "ROYAL": ["Fernanda Soto", "Daniel Ortega"]
+  "0001": ["Edith", "Julian", "Israel", "Said", "Tiana", "Bebingo"],
+  "0002": ["Monica", "Daniel", "Ivanna"],
+  "0003": ["Lucía Torres"],
+  "0004": ["Marco Díaz", "Carmen Silva"],
+  "0005": ["Pedro Ramírez"],
+  "0006": ["Fernanda Soto", "Daniel Ortega"]
   // Puedes agregar muchos más...
 };
